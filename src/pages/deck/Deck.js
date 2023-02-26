186aa9d3256b05417045310654c000a26fe36ca8
mrgrinsven <mrgrinsven@gmail.com>
2023-02-26 11:44:15 +0100

    Page not found and css update

--- src/pages/deck/Deck.js
+++ src/pages/deck/Deck.js
@@ -10,30 +10,33 @@ const Deck = () => {
     const {deckList} = useContext(DeckContext)
 
     return (
-        <div className="deck-cards-container">
+        <div className="inner-container">
             <DeckManagement/>
-            {Object.keys(deckList).length > 0  &&
-                <>
-                    {Object.keys(deckList).map((cardNames) => {
-                        const cardKeys = deckList[cardNames].cardIds
-                        return(
-                            Object.keys(deckList[cardNames].cardIds).map((card) => {
-                                return (
-                                    <Card
-                                        className={cardKeys[card].cardData.supertypes? (cardKeys[card].cardData.supertypes.includes('Basic')? 'basic-land' : null) : null }
-                                        cardImage={cardKeys[card].cardData.imageUrl}
-                                        key={cardKeys[card].cardData.id}
-                                        cardId={cardKeys[card].cardData.id}
-                                        cardInfo={cardKeys[card].cardData}
-                                        cardName={cardKeys[card].cardData.name}
-                                        supertypes={cardKeys[card].cardData.supertypes}
-                                    />
-                                );
-                            })
-                        );
-                    })}
-                </>
-            }
+            <div className="deck-cards-container">
+
+                {Object.keys(deckList).length > 0 &&
+                    <>
+                        {Object.keys(deckList).map((cardNames) => {
+                            const cardKeys = deckList[cardNames].cardIds
+                            return (
+                                Object.keys(deckList[cardNames].cardIds).map((card) => {
+                                    return (
+                                        <Card
+                                            className={cardKeys[card].cardData.supertypes ? (cardKeys[card].cardData.supertypes.includes('Basic') ? 'basic-land' : null) : null}
+                                            cardImage={cardKeys[card].cardData.imageUrl}
+                                            key={cardKeys[card].cardData.id}
+                                            cardId={cardKeys[card].cardData.id}
+                                            cardInfo={cardKeys[card].cardData}
+                                            cardName={cardKeys[card].cardData.name}
+                                            supertypes={cardKeys[card].cardData.supertypes}
+                                        />
+                                    );
+                                })
+                            );
+                        })}
+                    </>
+                }
+            </div>
         </div>
     );
 };
--- src/pages/page-not-found/PageNotFound.js
+++ src/pages/page-not-found/PageNotFound.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
+
+import './PageNotFound.css'
 
 const PageNotFound = () => {
+    const navigate = useNavigate()
+    setTimeout(() => {
+        navigate('/')
+    }, 10000);
+
     return (
-        <div>
-            PAGE NOT FOUND
+        <div className="inner-container">
+            <div className="page-not-found-container">
+                <h1>PAGE NOT FOUND</h1>
+                <h2>YOU WILL BE REDIRECTED BACK TO THE HOMEPAGE</h2>
+            </div>
         </div>
     );
 };
